6d85bf9f45bafde5524ecc11c36249c4ee3525ad
Fabian Ferno <fabianferno@gmail.com>
2021-12-08 12:58:35 +0000

    Add scan endpoint

--- routes/users.py
+++ routes/users.py
@@ -5,40 +5,28 @@ from config import mysql
 from flask import jsonify, request
 
 
-@app.route('/users', methods=['POST', 'GET'])
+@app.route('/scan', methods=['POST', 'GET'])
 def insert_cars():
     # Sample route with a method handlers and a view function
     try:
         _form = request.form
         _name = _form['name']
-        _email = _form['email']
+        _rollno = _form['rollno']
+        _timestamp = _form['timestamp']
+        _temp = _form['temp']
 
-        if _name and _email and request.method == 'POST':
+        if _name and _rollno and _timestamp and _temp and request.method == 'POST':
             # insert record in database
             conn = mysql.connect()
             cursor = conn.cursor()
             cursor.execute(
-                f"INSERT INTO users(name,email) VALUES({_name}, {_email})")
+                f"INSERT INTO temperature(name,rollno,timestamp,temp) VALUES('{_name}', '{_email}',')")
             conn.commit()
             cursor.close()
             conn.close()
             res = jsonify('success')
             res.status_code = 200
             return res
-
-        elif request.method == 'GET':
-            # Get record in database
-            _email = request.args["email"]
-            conn = mysql.connect()
-            cursor = conn.cursor()
-            cursor.execute(
-                f"SELECT * FROM `users` WHERE email='{_email}'")
-            res = cursor.fetchall()
-            cursor.close()
-            conn.close()
-            res.status_code = 200
-            return jsonify(res)
-
         else:
             return forbidden()
 
