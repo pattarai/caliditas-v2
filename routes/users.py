238ff67d83ed598cca6805b5dbdd63176fd36b11
Pinto <pintoinfant5650@gmail.com>
2021-12-08 21:09:37 +0530

    inserting into the db

--- routes/users.py
+++ routes/users.py
@@ -5,33 +5,29 @@ from config import mysql
 from flask import jsonify, request
 
 
-@app.route('/scan', methods=['POST'])
+@app.route('/scan', methods=['POST','GET'])
 def insert_temp():
-    # Sample route with a method handlers and a view function
     try:
-        _form = request.form
-        
-        _fname = _form['fname']
-        _lname = _form['lname']
-        _regno = _form['regno']
-        _timestamp = _form['timestamp']
-        _deviceId = _form['deviceId']
-        _temp = _form['temp']
+        _fname = request.args.get('fname')
+        _lname = request.args.get('lname')
+        _regno = request.args.get('regno')
+        _deviceId = request.args.get('deviceId')
+        _temp = request.args.get('temp')
         
 
-        if  _fname and _lname and _regno and _timestamp and _deviceId and _temp and request.method == 'POST':
-            # insert record in database
+        if  _fname and _lname and _regno  and _deviceId and _temp and request.method == 'POST':
             conn = mysql.connect()
             cursor = conn.cursor()
-            cursor.execute(f"INSERT INTO users(f_name,l_name,reg_no,timestamp,dev_id,temp) VALUES('{_fname}', '{_lname}','{_regno}','{_timestamp}','{_deviceId}','{_temp}')")
+            cursor.execute(f"INSERT INTO users(f_name,l_name,reg_num,dev_id,temp) VALUES('{_fname}','{_lname}','{_regno}','{_deviceId}','{_temp}')")
             conn.commit()
             cursor.close()
             conn.close()
-            res = jsonify('success')
+            res = jsonify({"message" : 'success'})
             res.status_code = 200
             return res
         else:
-            return forbidden()
+            return jsonify({"message" : 'failed'})
 
     except Exception as e:
         print(e)
+        return jsonify({"message" : 'failed in exception'})
