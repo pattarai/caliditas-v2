ce9f233a5b05de87b05f2a03d6579d01aa7e5fda
Fabian Ferno <fabianferno@gmail.com>
2021-12-09 02:57:01 +0530

    Temperature working

--- boot.py
+++ boot.py
@@ -3,39 +3,21 @@
 # esp.osdebug(None)
 import uos
 import machine
+import network
 # uos.dupterm(None, 1) # disable REPL on UART(0)
 import gc
 import webrepl
 
 
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+if not wlan.isconnected():
+    print('connecting to network...')
+    wlan.connect('Phantom', '8754462663')
+    while not wlan.isconnected():
+        pass
+print('network config:', wlan.ifconfig())
+
 # Start Webrepl
 webrepl.start()
 gc.collect()
-
-from machine import I2C, Pin, SoftI2C
-import urequests as requests
-import ssd1306  # Import OLED driver
-import mlx90614  # Melexis MLX90614 IR temperature sensor driver
-
-
-# Read Temperature
-i2cForIR = I2C(scl=Pin(5), sda=Pin(4))
-i2cForOled = SoftI2C(scl=Pin(0), sda=Pin(2))
-tempSensor = mlx90614.MLX90614(i2cForIR)
-
-
-# 128 vw x 64 vh display with hardware I2C
-oled = ssd1306.SSD1306_I2C(128, 64, i2cForOled)
-
-while True:
-    temperature = tempSensor.read_object_temp()
-    oled.text(f'Temp:{temperature}')
-    oled.show()
-    # print(tempSensor.read_ambient_temp(), tempSensor.read_object_temp())
-    response = requests.post("https://caliditas.herokuapp.com/scan", data={
-        "temp": str(temperature),
-        "rollNo": "19IT049",
-        "deviceId": "2"
-    })
-    oled.text(f'Temp:{temperature}\n|res:{response.text}')
-    time.sleep_ms(500)
--- main.py
+++ main.py
@@ -1,21 +1,41 @@
-# Complete project details at https://RandomNerdTutorials.com
 
-from machine import Pin, SoftI2C
-import ssd1306
-from time import sleep
+from machine import I2C, Pin, SoftI2C
+import urequests as requests
+import ssd1306  # Import oledDisplay driver
+import mlx90614  # Melexis MLX90614 IR temperature sensor driver
+import time
 
-# ESP32 Pin assignment
-i2c = SoftI2C(scl=Pin(22), sda=Pin(21))
 
-# ESP8266 Pin assignment
-#i2c = SoftI2C(scl=Pin(5), sda=Pin(4))
+try:
+    i2c = I2C(scl=Pin(5), sda=Pin(4))
 
-oled_width = 128
-oled_height = 64
-oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
+    oledDisplay = ssd1306.SSD1306_I2C(
+        128, 64, i2c)  # 128 vw x 64 vh display
+    tempSensor = mlx90614.MLX90614(i2c)
 
-oled.text('Hello, World 1!', 0, 0)
-oled.text('Hello, World 2!', 0, 10)
-oled.text('Hello, World 3!', 0, 20)
+    while True:
+        try:
+            oledDisplay.fill(0)
+            temperature = tempSensor.read_object_temp()
+            temperature = (float(temperature) * 9 / 5) + 32 + 2
+            temperature = str(temperature)
+            print(temperature)
+            oledDisplay.text(temperature, 35, 20)
+            oledDisplay.show()
 
-oled.show()
+            # print(tempSensor.read_ambient_temp(), tempSensor.read_object_temp())
+            try:
+                response = requests.post("https://caliditas.herokuapp.com/scan", data={
+                    "temp": str(temperature),
+                    "rollno": "19IT049",
+                    "deviceId": "2"
+                })
+                oledDisplay.text(response.text, 0, 0, 25)
+                oledDisplay.show()
+            except:
+                print("POST ERROR")
+        except:
+            print("DISPLAY ERROR")
+        time.sleep_ms(500)
+except:
+    print("LIB IMPORT ERROR")
