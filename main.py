86165835dcae0d0e27a2e6d15ec793ae0987940d
Fabian Ferno <fabianferno@gmail.com>
2021-12-25 04:44:08 +0530

    Code cleanup

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+SSID = "PhantomNode01"
+WIFI_PASS = "skywalker"
+
+
 try:
     import mlx90614  # Melexis MLX90614 IR temperature sensor driver
     from hcsr04 import HCSR04  # HC-SR04 Ultrasonic Sensor Driver
@@ -21,22 +25,23 @@ try:
                         try:
                             try:
                                 i2c = machine.SoftI2C(scl=machine.Pin(15), sda=machine.Pin(
-                                    14),freq=50000)
+                                    14), freq=50000)
                                 try:
                                     tempSensor = mlx90614.MLX90614(i2c)
-                                    
+
                                     try:
                                         oledDisplay = ssd1306.SSD1306_I2C(
                                             128, 64, i2c)  # 128 vw x 64 vh display
                                         try:
                                             try:
-                                                # Logo in Byte Array Format
+                                                # Pattarai Logo in Byte Array Format
                                                 buffer = bytearray(b'xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xc0\x03\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xfc\x00\x00\x3f\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xf0\x00\x00\x0f\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xc0\x00\x00\x03\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xfc\x00\x00\x00\x00\x3f\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xf8\x00\x01\x80\x00\x1f\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xf0\x00\x01\x80\x00\x0f\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xe0\x00\x01\x80\x00\x07\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xc0\x00\x01\x80\x00\x03\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\x80\x00\x01\x80\x00\x01\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\x00\x00\x01\x80\x00\x00\xff\xff\xff\xff\xff\xff\xff\xff\xff\xfe\x00\x40\x01\x80\x02\x00\x7f\xff\xff\xff\xff\xff\xff\xff\xff\xfe\x00\x60\x00\x00\x06\x00\x7f\xff\xff\xff\xff\xff\xff\xff\xff\xfc\x00\x30\x00\x00\x0c\x00\x3f\xff\xff\xff\xff\xff\xff\xff\xff\xfc\x00\x18\x03\xc0\x18\x00\x3f\xff\xff\xff\xff\xff\xff\xff\xff\xf8\x00\x0c\x1f\xf8\x30\x00\x1f\xff\xff\xff\xff\xff\xff\xff\xff\xf8\x00\x00\x7c\x3e\x00\x00\x1f\xff\xff\xff\xff\xff\xff\xff\xff\xf0\x00\x00\xe0\x07\x00\x00\x0f\xff\xff\xff\xff\xff\xff\xff\xff\xf0\x00\x01\xc0\x01\x80\x00\x0f\xff\xff\xff\xff\xff\xff\xff\xff\xe0\x00\x03\x00\x00\xc0\x00\x07\xff\xff\xff\xff\xff\xff\xff\xff\xe0\x00\x07\x00\x00\x60\x00\x07\xff\xff\xff\xff\xff\xff\xff\xff\xe0\x00\x06\x18\x0c\x60\x00\x07\xff\xff\xff\xff\xff\xff\xff\xff\xe0\x00\x0c\x38\x0c\x30\x00\x07\xff\xff\xff\xff\xff\xff\xff\xff\xc0\x00\x0c\x78\x0e\x30\x00\x03\xff\xff\xff\xff\xff\xff\xff\xff\xc0\x00\x0c\x78\x0e\x10\x00\x03\xff\xff\xff\xff\xff\xff\xff\xff\xc0\x00\x0c\x78\x0f\x18\x00\x03\xff\xff\xff\xff\xff\xff\xff\xff\xc0\x1f\x8c\x78\x1f\x19\xf8\x03\xff\xff\xff\xff\xff\xff\xff\xff\xc0\x1f\x8c\x7f\xff\x19\xf8\x03\xff\xff\xff\xff\xff\xff\xff\xff\xc0\x00\x0c\x7f\xfe\x30\x00\x03\xff\xff\xff\xff\xff\xff\xff\xff\xc0\x00\x0c\x7f\xfe\x30\x00\x03\xff\xff\xff\xff\xff\xff\xff\xff\xc0\x00\x06\x3f\xfc\x30\x00\x03\xff\xff\xff\xff\xff\xff\xff\xff\xc0\x00\x06\x1f\xfc\x60\x00\x03\xff\xff\xff\xff\xff\xff\xff\xff\xc0\x00\x03\x0f\xf8\x60\x00\x03\xff\xff\xff\xff\xff\xff\xff\xff\xc0\x00\x03\x0f\xf0\xc0\x00\x03\xff\xff\xff\xff\xff\xff\xff\xff\xc0\x00\x01\x8f\xf0\x80\x00\x03\xff\xff\xff\xff\xff\xff\xff\xff\xe0\x00\x01\x8f\xf1\x80\x00\x07\xff\xff\xff\xff\xff\xff\xff\xff\xe0\x00\x00\xcf\xf1\x00\x00\x07\xff\xff\xff\xff\xff\xff\xff\xff\xe0\x00\x0c\xcf\xf3\x30\x00\x07\xff\xff\xff\xff\xff\xff\xff\xff\xe0\x00\x18\x6f\xf3\x18\x00\x07\xff\xff\xff\xff\xff\xff\xff\xff\xf0\x00\x30\x6f\xf6\x0c\x00\x0f\xff\xff\xff\xff\xff\xff\xff\xff\xf0\x00\x60\x6f\xf6\x06\x00\x0f\xff\xff\xff\xff\xff\xff\xff\xff\xf8\x00\x40\x3f\xf6\x02\x00\x1f\xff\xff\xff\xff\xff\xff\xff\xff\xf8\x00\x00\x3f\xfc\x00\x00\x1f\xff\xff\xff\xff\xff\xff\xff\xff\xfc\x00\x00\x3f\xfc\x00\x00\x3f\xff\xff\xff\xff\xff\xff\xff\xff\xfc\x00\x00\x1f\xf8\x00\x00\x3f\xff\xff\xff\xff\xff\xff\xff\xff\xfe\x00\x00\x00\x00\x00\x00\x7f\xff\xff\xff\xff\xff\xff\xff\xff\xfe\x00\x00\x0f\xf0\x00\x00\x7f\xff\xff\xff\xff\xff\xff\xff\xff\xff\x00\x00\x00\x00\x00\x00\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\x80\x00\x0f\xf0\x00\x01\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xc0\x00\x07\xe0\x00\x03\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xe0\x00\x00\x00\x00\x07\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xf0\x00\x07\xe0\x00\x0f\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xf8\x00\x01\x80\x00\x1f\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xfc\x00\x00\x00\x00\x3f\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\x80\x00\x00\x01\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xf0\x00\x00\x0f\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xfc\x00\x00\x3f\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xc0\x03\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff')
 
                                                 fb = framebuf.FrameBuffer(
                                                     buffer, 128, 64, framebuf.MONO_HLSB)
                                                 oledDisplay.fill(0)
-                                                oledDisplay.framebuf.blit(fb, 0, -10)
+                                                oledDisplay.framebuf.blit(
+                                                    fb, 0, -10)
                                                 oledDisplay.invert(0)
                                                 oledDisplay.show()
                                                 time.sleep_ms(2000)
@@ -51,26 +56,29 @@ try:
 
                                             except:
                                                 print("Logo Loading Error")
-                                                
+
                                             try:
                                                 oledDisplay.fill(0)
                                                 oledDisplay.text(
                                                     "WAITING FOR WIFI", 0, 20)
                                                 oledDisplay.text(
-                                                    "jesinthan", 20, 40)
+                                                    SSID, 20, 40)
                                                 oledDisplay.show()
-                                                wlan = network.WLAN(network.STA_IF)
+
+                                                wlan = network.WLAN(
+                                                    network.STA_IF)
                                                 wlan.active(True)
                                                 if not wlan.isconnected():
-                                                    print('connecting to network...')
-                                                    wlan.connect('jesinthan', 'jesinthan')
+                                                    print(
+                                                        'connecting to network...')
+                                                    wlan.connect(
+                                                        SSID, WIFI_PASS)
                                                     while not wlan.isconnected():
                                                         pass
                                                 if wlan.isconnected():
                                                     print('connected')
                                             except:
                                                 print("WIFI MANAGER ERROR")
-                                            
 
                                             try:
                                                 buzzer = machine.Pin(
@@ -83,43 +91,37 @@ try:
                                             try:
                                                 while True:
                                                     try:
-                                                        proximitySensor = HCSR04(trigger_pin=2, echo_pin=13, echo_timeout_us=10000)
+                                                        proximitySensor = HCSR04(
+                                                            trigger_pin=2, echo_pin=13, echo_timeout_us=10000)
                                                     except:
-                                                        print("PROXIMITY SENSOR INIT ERROR")
+                                                        print(
+                                                            "PROXIMITY SENSOR INIT ERROR")
                                                     try:
                                                         distance = proximitySensor.distance_cm()
                                                         if distance < 40 and distance > 30:
                                                             try:
-                                                                temperature = tempSensor.read_object_temp()
                                                                 # Fahrenheit Conversion & Temperature Offset for Distance
                                                                 temperature = (
-                                                                    (float(temperature) * 9 / 5) + 32) + 12
-                                                                print(temperature)
-                                                                
-                                                                oledDisplay.fill(0)
+                                                                    (float(tempSensor.read_object_temp()) * 9 / 5) + 32) + 12
+                                                                print(
+                                                                    temperature)
+
+                                                                oledDisplay.fill(
+                                                                    0)
                                                                 oledDisplay.text(
                                                                     str(temperature), 35, 0)
                                                                 oledDisplay.show()
 
                                                                 if temperature > 99 and temperature < 120:
-                                                                    #buzzer.value(1)
+                                                                    # buzzer.value(1)
                                                                     oledDisplay.text(
                                                                         "ALERT!!", 20, 20)
                                                                     oledDisplay.text(
-                                                                        "GO BACK FOR PIC", 0, 40)
+                                                                        "STEP BACK FOR PIC", 0, 40)
                                                                     oledDisplay.show()
 
                                                                     try:  # Insert Temperature to DB
-                                                                        oledDisplay.fill(
-                                                                            0)
-                                                                        oledDisplay.text(
-                                                                            "INSERTING", 20, 20)
-                                                                        oledDisplay.text(
-                                                                            "TO DB", 0, 40)
-                                                                        oledDisplay.show()
-                                                                        time.sleep_ms(
-                                                                            1000)
-                                                                       
+
                                                                         oledDisplay.fill(
                                                                             0)
                                                                         oledDisplay.text(
@@ -131,11 +133,12 @@ try:
                                                                             500)
 
                                                                     except:
-                                                                        print("API ERROR")
+                                                                        print(
+                                                                            "API ERROR")
                                                                 else:
-                                                                    #buzzer.value(0)
+                                                                    # buzzer.value(0)
                                                                     oledDisplay.text(
-                                                                        "YOU MAY PASS", 15, 20)
+                                                                        "YOU SHALL PASS", 15, 20)
                                                                     oledDisplay.show()
                                                                     time.sleep_ms(
                                                                         1000)
@@ -150,10 +153,11 @@ try:
                                                             oledDisplay.show()
                                                             time.sleep_ms(500)
                                                     except:
-                                                        print("PROXIMITY SENSOR VALUE ERROR")
+                                                        print(
+                                                            "PROXIMITY SENSOR VALUE ERROR")
                                             except:
                                                 print(
-                                                    "Error in Reading Proximity")
+                                                    "Error in Reading Proximity Sensor")
                                                 time.sleep_ms(2000)
 
                                         except:
